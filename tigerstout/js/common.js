f87ae3257ee73ca2bea0eaee6315d7bfbfa1eceb
neconi <neconin@gmail.com>
2019-07-02 11:42:51 +0900

    切り戻し
    問題あった

--- tigerstout/js/common.js
+++ tigerstout/js/common.js
@@ -5,7 +5,7 @@ $(function(){
 	//ページ読み込み時初期化
 	$("nav").addClass("nav-init");
 	$("footer").after('<div class="dark"></div>');
-	$(".nav-open").text("＋").show();
+	$(".nav-open").text("＋");
 	
 	//メニューを閉じる
 	$(".nav-close").click(function(){
